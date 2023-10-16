c563647bea220fb0abff423b17ebe2cf32332af3
= <=>
2023-10-17 02:48:59 +0800

    web1.0

--- docs/assets/index.e227527c.js
+++
@@ -1,1 +0,0 @@
-import{i}from"./index.18d05f4c.js";import{j as e}from"./vendor.0b417fb8.js";var t=()=>e("div",{style:{height:"100vh",position:"relative",display:"flex",flexDirection:"column"},children:e(i,{})});export{t as default};
